96e1f626bc3f32d4b2808fcd959c24a584c1eb13
heartplay <168409591+heartplay@users.noreply.github.com>
2024-05-28 17:30:37 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,37 @@
+// let header = $('.header')
+// $()const header = document.getElementById('header')
+
+// $(function headerScroll () {
+//     if (header.scrollTop > 1) {
+//         header.addClass('header-fixed')
+//     }
+//     else {
+//         header.removeClass('header-fixed')
+//     }
+// })
+
+// $(function() {
+//     let header = $('.header');
+     
+//     $(window).scroll(function() {
+//       if($(this).scrollTop() > 1) {
+//        header.addClass('header-fixed');
+//       } else {
+//        header.removeClass('header-fixed');
+//       }
+//     });
+//    });
+
+// $(window).scroll(function () {
+//     if ($(window).scrollTop() > 20) {
+//         $('.header').classList.add('header-fixed');
+//     }
+//     else {
+//         $('.header').classList.remove('header-fixed')
+//     }
+// })
+
+// const resultElement = document.getElementById('result')
+// const Btn = document.getElementById('submit')
+
+
